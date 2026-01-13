887d2e3028fd6ec960be377fae617561b0a6dbea
Svetoslav Mitev <svetliomitev@gmail.com>
2026-01-13 17:23:36 +0200

    Update install.php
    Update version number to match the latest and initial release to the branch code.

--- install.php
+++ install.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/lib.php';
 mc_security_headers();
 mc_session_start();
 
-$APP_VERSION = '1.5.86';
+$APP_VERSION = '1.5.85';
 
 /* =========================
    INSTALLER WIZARD (responsive)
